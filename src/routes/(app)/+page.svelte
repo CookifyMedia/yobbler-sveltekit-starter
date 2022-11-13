a4a36d6e5176d4da751c9c6a2322cef4cc4fc75e
CookifyMedia <sportbilsgiganten@gmail.com>
2022-11-13 16:55:17 +0100

    added dummy info on startpage

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,18 +1,53 @@
 <script lang="ts">
- import { siteDescription } from '$lib/config'   
-	import type { PageData } from './$types';
+ import { siteDescription } from '$lib/config'  
 
- export let data: PageData
+
 </script>
 
 <svelte:head>
-	<title>Yobbler</title>
+	<title>Yobbler starter template</title>
 	<meta data-key="description" name={siteDescription}>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation </p>
 
-{#if data.user}
-	{data?.user?.firstName} 
-{/if}
+	<div class="hero py-20 bg-surface-1">
+		<div class="hero-content flex-col lg:flex-row-reverse">
+		  <img src="https://placeimg.com/260/400/arch" alt="hero" class="max-w-sm rounded-lg shadow-2xl" />
+		  <div>
+			<h1 class="text-5xl font-bold">Welcome to Yobbler SvelteKit Starter</h1>
+			<p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+			<button class="btn btn-primary">Get Started</button>
+		  </div>
+		</div>
+	</div>
+
+	<div class="hero min-h-screen bg-surface-1">
+		<div class="hero-content flex-col lg:flex-row-reverse">
+		  <div class="text-center lg:text-left">
+			<h1 class="text-5xl font-bold">Login now!</h1>
+			<p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+		  </div>
+		  <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+			<div class="card-body">
+			  <div class="form-control">
+				<label class="label">
+				  <span class="label-text">Email</span>
+				</label>
+				<input type="text" placeholder="email" class="input input-bordered" />
+			  </div>
+			  <div class="form-control">
+				<label class="label">
+				  <span class="label-text">Password</span>
+				</label>
+				<input type="text" placeholder="password" class="input input-bordered" />
+				<label class="label">
+				  <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+				</label>
+			  </div>
+			  <div class="form-control mt-6">
+				<button class="btn btn-primary">Login</button>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
